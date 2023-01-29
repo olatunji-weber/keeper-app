bc7105c23dfb5b74b8a0f9b22e3ac7c6a9201fb3
olatunji-weber <olatunji.weber@gmail.com>
2023-01-29 05:23:37 +0200

    The very first commit for the Keeper App

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Header(){
+    return <header>
+        <h1>Keeper: This is some Awesome shit bro...</h1>
+    </header>;
+}
+
+export default Header;
---
+++ src/components/Note.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function note(){
+    return(
+        <div className="note">
+            <h1>Title - My Title is so lovely</h1>
+            <p>Content - My content is incontestable...</p>
+        </div>
+    );
+}
+
+export default note;
